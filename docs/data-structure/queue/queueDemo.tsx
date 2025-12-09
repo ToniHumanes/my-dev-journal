f41a069504a93da1d3bf839b7c480e045064e17e
Antonio Humanes <antoniohumanespacheco@gmail.com>
2025-12-10 00:10:18 +0100

    feat: Add Queue

---
+++ docs/data-structure/queue/queueDemo.tsx
@@ -0,0 +1,83 @@
+import { Button } from "@site/src/components/core/Button/Button";
+import { Label } from "@site/src/components/core/Label";
+import { useCallback, useEffect, useState } from "react";
+
+class Queue<T> {
+  private items: T[] = [];
+
+  enqueue(element: T): void {
+    this.items.push(element);
+  }
+
+  dequeue(): T | undefined {
+    return this.items.shift();
+  }
+
+  peek(): T | undefined {
+    return this.items[0];
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+
+  getItems(): T[] {
+    return this.items;
+  }
+}
+
+export const QueueDemo = () => {
+  const [queue] = useState(new Queue<string>());
+  const [queueItems, setQueueItems] = useState<string[]>([]);
+
+  const removeQueueItem = useCallback(() => {
+    if (queue.isEmpty()) return;
+    queue.dequeue();
+    setQueueItems([...queue.getItems()]);
+  }, [queue]);
+
+  const addQueueItem = () => {
+    queue.enqueue(new Date().getTime().toString());
+    setQueueItems([...queue.getItems()]);
+  };
+
+  return (
+    <>
+      <div style={{ display: "flex", gap: "8px", marginBottom: "1rem" }}>
+        <Button
+          label="Crear notificación en cola"
+          onClick={() => {
+            addQueueItem();
+          }}
+        />
+
+        <Button
+          color="secondary"
+          label="Borrar notificación en cola"
+          onClick={() => {
+            removeQueueItem();
+          }}
+        />
+      </div>
+
+      <section style={{ marginTop: "8px" }}>
+        <p>Primera notificación (actual) en la cola: {queue.peek()}</p>
+        <p>Número de Notificaciones en la cola: {queue.size()}</p>
+        <p>Notificaciones:</p>
+        {queueItems.map((item: string) => (
+          <div key={item} style={{ marginBottom: "8px" }}>
+            <Label color="yellow" text={`Nueva notificación con id: ${item}`} />
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
